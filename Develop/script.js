6a5ed0ec508645bb0a9e540b109f138e4c208d00
Karolyn Seredick <kseredick95@gmail.com>
2020-03-18 17:05:15 -0400

    Added password if/else function

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,75 @@
 // Assignment Code
-var numOfChar = window.prompt("How many characters would you like in your password?")
+
 var generateBtn = document.querySelector("#generate");
 
-
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var numChar = window.prompt("How many characters would you like in your password? Must enter between 8 and 128.")
 
-  passwordText.value = password;
+  while (numChar < 8 || numChar > 128){
+    numChar = window.prompt("Invalid number. How many characters would you like in your password? Must enter between 8 and 128.")
+  }
 
-}
+  var capLetter = window.confirm("Would you like to include capital letters?")
+
+  var specialChar = window.confirm("Would you like to include special characters?")
+  
+  var password = generatePassword(numChar, capLetter, specialChar);
+
+
+    var passwordText = document.querySelector("#password");
+
+    function generatePassword(numChar, capLetter, specialChar){
+      //Capital and special characters
+      if (capLetter == true && specialChar == true){
+        var charSet = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+        var retVal = ""
+        
+        for (var i = 0, l = charSet.length; i < numChar; i++){
+          retVal += charSet.charAt(Math.floor(Math.random()*l));
+
+        }
+
+      }
+      //Only special characters
+      else if (capLetter == true && specialChar == false){
+        var charSet = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var retVal = ""
+        
+        for (var i = 0, l = charSet.length; i < numChar; i++){
+          retVal += charSet.charAt(Math.floor(Math.random()*l));
+
+        }
+
+      }
+      //Only capital letters
+      else if (capLetter == false && specialChar == true){
+        var charSet = "abcdefghijklmnopqurstuvwxyz0123456789!@#$%^&*()"
+        var retVal = ""
+        
+        for (var i = 0, l = charSet.length; i < numChar; i++){
+          retVal += charSet.charAt(Math.floor(Math.random()*l));
+
+        }
+
+      }
+      //Only lowercase and numbers
+      else{
+        var charSet = "abcdefghijklmnopqurstuvwxyz0123456789"
+        var retVal = ""
+        
+        for (var i = 0, l = charSet.length; i < numChar; i++){
+          retVal += charSet.charAt(Math.floor(Math.random()*l));
+
+        }
+
+      }
+
+      return retVal;
+    }
+    
+    passwordText.value = password;
+
+  }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
